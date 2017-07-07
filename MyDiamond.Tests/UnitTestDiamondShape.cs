f3ac36c85b79f70b1a369d50c0239aeb30c0e5e6
ss223ck <ss223ck@student.lnu.se>
2017-07-07 16:56:48 +0200

    First test failed as sopposed to

--- MyDiamond.Tests/UnitTestDiamondShape.cs
+++ MyDiamond.Tests/UnitTestDiamondShape.cs
@@ -7,9 +7,15 @@ namespace MyDiamond.Tests
     public class UnitTestDiamondShape
     {
         [Fact]
-        public void TestProjectReference()
+        public void TestConstructor()
         {
-            DiamondShape diamondShape = new DiamondShape();
+            int expected = 11;
+            int actual;
+            DiamondShape diamondShape = new DiamondShape(expected);
+
+            actual = diamondShape.DiamondSize;
+
+            Assert.Equal(expected, actual);
         }
     }
 }
