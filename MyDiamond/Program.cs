c5d3f7ebaa7c88811399cc535d2bce0efad5cef1
ss223ck <ss223ck@student.lnu.se>
2017-07-07 16:32:30 +0200

    Initiated the main project and the test project

---
+++ MyDiamond/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
