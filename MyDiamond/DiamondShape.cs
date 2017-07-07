4134ca3f16717eb8f6fc49ca5fed1075e42fafb0
ss223ck <ss223ck@student.lnu.se>
2017-07-07 16:48:49 +0200

    Added the first interface of functionality of the class DiamondShape.

--- MyDiamond/DiamondShape.cs
+++ MyDiamond/DiamondShape.cs
@@ -4,5 +4,29 @@ namespace MyDiamond
 {
     public class DiamondShape
     {
+        private int _diamondSize;
+
+        public int DiamondSize { get {return _diamondSize;} }
+
+        public DiamondShape(int diamondSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string createDiamond()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string createRow(int rowCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    
     }
 }
