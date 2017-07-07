46ad288b11e20a84625b10b0618b824578ba8bf2
ss223ck <ss223ck@student.lnu.se>
2017-07-07 16:46:00 +0200

    Added the test class for DiamondShape.cs

---
+++ MyDiamond.Tests/UnitTestDiamondShape.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using MyDiamond;
+
+namespace MyDiamond.Tests
+{
+    public class UnitTestDiamondShape
+    {
+        [Fact]
+        public void TestProjectReference()
+        {
+            DiamondShape diamondShape = new DiamondShape();
+        }
+    }
+}
